19d3069ed82e57a4649dfac0b79184e9c8e73a13
Crowdbotics <team@crowdbotics.com>
2020-05-17 02:37:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/unicorn_maze_17084/MainActivity.java
@@ -0,0 +1,15 @@
+package com.unicorn_maze_17084;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "unicorn_maze_17084";
+    }
+}
